8a2d6ebb5a5ed06feefe46db19314ecf13be8b3a
Joe Vincent <joevincentkc@gmail.com>
2025-04-23 09:06:17 -0700

    Increment version in setup.py
    Need to update version so that the updates can be pushed to PyPI.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
    name="binomial_cis",
-   version='0.0.11',
+   version='0.0.12',
    author="Joe Vincent",
    description="Confidence intervals for binomial distributions.",
    packages=find_packages(),
